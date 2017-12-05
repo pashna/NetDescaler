8a8e1b17c8f4040997062f58f0d19ff170743621
Pavel Kochetkov <pavel.kochetkov@careem.com>
2017-12-05 23:53:57 +0400

    +controller

--- first_test.py
+++ first_test.py
@@ -24,6 +24,7 @@ def perfTest():
     "Create network and run simple performance test"
     topo = SingleSwitchTopo(n=4)
     net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
+    c2 = RemoteController('controller', ip='127.0.0.1', port=6633)
     net.start()
     print("Dumping host connections")
     dumpNodeConnections( net.hosts )

f7fc31145d3398ae229163d8c617e38e47eacbb4
Pavel Kochetkov <pavel.kochetkov@careem.com>
2017-12-05 23:51:52 +0400

    DITG

---
+++ first_test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.node import OVSSwitch, Controller, RemoteController
+
+
+class SingleSwitchTopo( Topo ):
+    "Single switch connected to n hosts."
+    def build( self, n=2 ):
+        switch = self.addSwitch( 's1' )
+        for h in range(n):
+            # Each host gets 50%/n of system CPU
+            host = self.addHost( 'h%s' % (h + 1),
+                             cpu=.5/n )
+
+            self.addLink( host, switch, bw=20)
+
+def perfTest():
+    "Create network and run simple performance test"
+    topo = SingleSwitchTopo(n=4)
+    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
+    net.start()
+    print("Dumping host connections")
+    dumpNodeConnections( net.hosts )
+    print("Testing network connectivity")
+    net.pingAll()
+    print("Testing bandwidth between h1 and h4")
+    h1, h4 = net.get('h1', 'h4')
+    h1.sendCmd('./D-ITG-2.8.1-r1023/bin/ITGRecv')
+    h4.sendCmd('./D-ITG-2.8.1-r1023/bin/ITGSend –T UDP  –a 10.0.0.2 –c 100 –C 10 –t 15000 -l sender.log –x receiver.log ')
+
+    results = {}
+    for h in [h1, h4]:
+        results[h.name] = h.waitOutput()
+
+    print(results)
+    net.iperf((h1, h4))
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    perfTest()
